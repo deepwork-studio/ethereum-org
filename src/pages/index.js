0b080fb2a116e0f4b8ad624f55caf9eefeab6986
Jeff Reiner <theundeadghost@gmail.com>
2019-03-29 16:47:28 +0100

    :sparkles: start on home

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,16 +1,35 @@
 import React from "react";
-import { Heading, Text } from "rebass";
+import { Flex } from "rebass";
 
 import Layout from "../components/Layout";
 import SEO from "../components/SEO";
+import Card from "../components/Card";
+import Paragraph from "../components/Paragraph";
+import Header from "../components/Header";
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    
-    <Heading as="h1">Hi peeps</Heading>
-    <Text as="p">Welcome to your new Deep Work Studio site.</Text>
-    <Text as="p">Let's get building!</Text>
+    <Flex>
+      <Card flex={1} bg="primary" gradient="richblue" pt={40 + 6} pb={4} px={4}>
+        <Header color="richblue">Build Unstoppable Organisations</Header>
+        <Paragraph fontSize={3} mb={0} mt={2}>
+          Developer Tools
+        </Paragraph>
+      </Card>
+      <Card flex={1} bg="primary" gradient="gossamer" pt={40 + 6} pb={4} px={4}>
+        <Header color="gossamer">Why Ethereum</Header>
+        <Paragraph fontSize={3} mb={0} mt={2}>
+          Learn
+        </Paragraph>
+      </Card>
+      <Card flex={1} bg="primary" gradient="tapestry" pt={40 + 6} pb={4} px={4}>
+        <Header color="tapestry">News and Community</Header>
+        <Paragraph fontSize={3} mb={0} mt={2}>
+          Newsroom
+        </Paragraph>
+      </Card>
+    </Flex>
   </Layout>
 );
 
