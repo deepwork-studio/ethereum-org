622f6bad80f99cc2000ddd048b8e073fa1bbaad2
Jeff Reiner <theundeadghost@gmail.com>
2019-04-15 21:04:51 +0200

    :sparkles: better nav

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,67 +1,71 @@
 import React from "react";
 import { Box, Flex } from "rebass";
 import PropTypes from "prop-types";
+import useMedia from "react-use/lib/useMedia";
 
 import Caps from "../Caps";
 import { GradientBar } from "../Card";
 import { InternalLink } from "../Links";
-import Container from "../Container";
 
-const Nav = ({ page }) => (
-  <Box
-    css={{
-      position: "fixed",
-      bottom: 0,
-      left: 0,
-      right: 0,
-      zIndex: 99999
-    }}
-  >
-    <Container>
-      <Flex bg="primary" color="text">
-        <Flex
-          flex={1}
-          alignItems="center"
-          justifyContent="center"
-          css={{ position: "relative" }}
-        >
-          {page === "developers" && <GradientBar gradient="richblue" />}
-          <InternalLink to="/developers/">
-            <Caps m={0} py={4}>
-              Developers
-            </Caps>
-          </InternalLink>
+const Nav = ({ page }) => {
+  const isNotMobile = useMedia('(min-width: 40em)');
+
+  return (
+    <Box
+      css={{
+        position: "fixed",
+        bottom: 0,
+        left: 0,
+        right: 0,
+        zIndex: 99999
+      }}
+    >
+      <Box css={{ maxWidth: 960 - 32 * 2 }} mx="auto">
+        <Flex bg="primary" color="text">
+          <Flex
+            flex={1}
+            alignItems="center"
+            justifyContent="center"
+            css={{ position: "relative" }}
+          >
+            {page === "developers" && <GradientBar gradient="richblue" />}
+            <InternalLink to="/developers/">
+              <Caps m={0} py={4}>
+                {isNotMobile ? "Developers": "Devs"}
+              </Caps>
+            </InternalLink>
+          </Flex>
+          <Flex
+            flex={1}
+            alignItems="center"
+            justifyContent="center"
+            css={{ position: "relative" }}
+          >
+            {page === "learn" && <GradientBar gradient="gossamer" />}
+            <InternalLink to="/learn/">
+              <Caps m={0} py={4}>
+                Learn
+              </Caps>
+            </InternalLink>
+          </Flex>
+          <Flex
+            flex={1}
+            alignItems="center"
+            justifyContent="center"
+            css={{ position: "relative" }}
+          >
+            {page === "news" && <GradientBar gradient="tapestry" />}
+            <InternalLink to="/news/">
+              <Caps m={0} py={4}>
+                News
+              </Caps>
+            </InternalLink>
+          </Flex>
         </Flex>
-        <Flex
-          flex={1}
-          alignItems="center"
-          justifyContent="center"
-          css={{ position: "relative" }}
-        >
-          {page === "learn" && <GradientBar gradient="gossamer" />}
-          <InternalLink to="/learn/">
-            <Caps m={0} py={4}>
-              Learn
-            </Caps>
-          </InternalLink>
-        </Flex>
-        <Flex
-          flex={1}
-          alignItems="center"
-          justifyContent="center"
-          css={{ position: "relative" }}
-        >
-          {page === "news" && <GradientBar gradient="tapestry" />}
-          <InternalLink to="/news/">
-            <Caps m={0} py={4}>
-              News
-            </Caps>
-          </InternalLink>
-        </Flex>
-      </Flex>
-    </Container>
-  </Box>
-);
+      </Box>
+    </Box>
+  );
+};
 
 Nav.propTypes = {
   page: PropTypes.oneOf("developers", "learn", "news")
